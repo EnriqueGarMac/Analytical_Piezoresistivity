0cf4187fdbe2fa391b733e252b754981bf76ed60
Enrique García Macías <enriquegm@go.ugr.es>
2021-06-02 21:49:03 +0200

    Mod_1

--- MATLAB/functions/equivalent_filler.m
+++ MATLAB/functions/equivalent_filler.m
@@ -11,12 +11,7 @@ sigmaL_EH=(L+2*t(1))*Sigma_int(1)*(sigmaL_CNT*rc^2+Sigma_int(1)*(2*rc*t(1)+t(1)^
      
 % Conductive Networks
 % ================
-
-% sigmaT_CN=(Sigma_int(2)*(2*rc^2*sigmaT_CNT+2*rc*t(2)*(sigmaT_CNT+Sigma_int(2))+t(2)^2*(sigmaT_CNT+Sigma_int(2))))/(2*rc^2*Sigma_int(2)+2*rc*t(2)*(sigmaT_CNT+Sigma_int(2))+t(2)^2*(sigmaT_CNT+Sigma_int(2)));
-% sigmaL_CN=(rc^2*sigmaL_CNT+2*rc*t(2)*Sigma_int(2)+t(2)^2*Sigma_int(2))/(rc+t(2))^2;
-% sigmaT_CN=Sigma_int(2)*((2*rc^2*sigmaT_CNT+(sigmaT_CNT+Sigma_int(2))*(t(2)^2+2*rc*t(2)))/(2*rc^2*Sigma_int(2)+(sigmaT_CNT+Sigma_int(2))*(t(2)^2+2*rc*t(2))));
-% sigmaL_CN=(sigmaL_CNT*rc^2+Sigma_int(2)*(2*rc*t(2)+t(2)^2))/(rc+t(2))^2;
-sigmaT_CN=(Sigma_int(2)/(L+2*t(2)))*(L*(2*rc^2*sigmaT_CNT+(sigmaT_CNT+Sigma_int(2))*(t(2)^2+2*rc*t(2)))/(2*rc^2*Sigma_int(2)+(sigmaT_CNT+Sigma_int(2))*(t(1)^2+2*rc*t(2)))+2*t(2));
-sigmaL_CN=(L+2*t(2))*Sigma_int(2)*(sigmaL_CNT*rc^2+Sigma_int(2)*(2*rc*t(2)+t(2)^2))/(2*sigmaL_CNT*rc^2*t(2)+2*Sigma_int(2)*(2*rc*t(1)+t(2)^2)*t(2)+Sigma_int(2)*L*(rc+t(2))^2);
+sigmaT_CN=(Sigma_int(2)/(L+2*t(2)))*(L*(2*rc^2*sigmaT_CNT+(sigmaT_CNT+Sigma_int(2))*(t(2)^2+2*rc*t(2)))/(2*rc^2*Sigma_int(2)+(sigmaT_CNT+Sigma_int(2))*(t(2)^2+2*rc*t(2)))+2*t(2));
+sigmaL_CN=(L+2*t(2))*Sigma_int(2)*(sigmaL_CNT*rc^2+Sigma_int(2)*(2*rc*t(2)+t(2)^2))/(2*sigmaL_CNT*rc^2*t(2)+2*Sigma_int(2)*(2*rc*t(2)+t(2)^2)*t(2)+Sigma_int(2)*L*(rc+t(2))^2);
  
 end
--- MATLAB/functions/interphase_CNT.m
+++ MATLAB/functions/interphase_CNT.m
@@ -4,7 +4,6 @@ function [Sigma_int_EH,t_EH,Sigma_int_CN,t_CN]=interphase_CNT(CNT_prop,f,fc,dco,
 format long
 
 d_CNT=CNT_prop(1);
-%L_CNT=CNT_prop(2)*updated_length(strain*0,poism);
 L_CNT=CNT_prop(2);
 rc=d_CNT/2;
 
@@ -13,8 +12,6 @@ rc=d_CNT/2;
 % Maximum possible distance that allows the tunneling penetration of
 % electrons
 dc = dco*10^(-9);
-% dc = 1.8*10^(-9)*updated_length(strain*0,poism)
-% dc= 1.8*10^(-9);
 % Planck constant
 hplanck = 6.626068*10^(-34);
 % Mass of an electron *)
@@ -26,13 +23,9 @@ Lambda=Lambdao*ee;
 
 % Initial calculations
 % ==================
-% Aspect ratio
-asp=L_CNT/d_CNT;
 % Contact area of CNTs
-% a_EH=(d_CNT)^2;
 a_EH=pi*(d_CNT/2)^2;
 a_CN=pi*(d_CNT/2)^2;
-% a_CN=(d_CNT)^2;
 
 
 % Percentage of percolated CNTs *)
@@ -43,7 +36,7 @@ Xi = (f^(1/3)-fc^(1/3))/(1-fc^(1/3));
 % ==================
 % CONDUCTIVE NETWORK:
 da=(fc/f)^(1/3)*dc;
-% Tunneling-type contact resistance between two CNTs 
+% Tunneling-type contact resistance between two CNTs
 Rint_CN = (da*hplanck^2/(a_CN*ee^2*(2*m*Lambda)^0.5))*exp((4*pi*da/hplanck)*(2*m*Lambda)^0.5);
 % Thickness of the interphase *)
 t_CN = da/2;    %* Power law relation after percolation
@@ -51,18 +44,18 @@ t_CN = da/2;    %* Power law relation after percolation
 Sigma_int_CN = da/(a_CN*Rint_CN);
 
 % ELECTRON HOPPING:
-% Tunneling-type contact resistance between two CNTs 
+% Tunneling-type contact resistance between two CNTs
 Rint_EH = (dc*hplanck^2/(a_EH*ee^2*(2*m*Lambda)^0.5))*exp((4*pi*dc/hplanck)*(2*m*Lambda)^0.5);
 % Thickness of the interphase *)
-t_EH = dc/2;    %* Constant distance before percolation
+t_EH = dc/2;    % Constant distance before percolation
 % Conductivity of the interphase
 Sigma_int_EH = dc/(a_EH*Rint_EH);
 
 
 
 if Xi>=0
-Sigma_int_EH=Sigma_int_CN;
-t_EH=t_CN;
+    Sigma_int_EH=Sigma_int_CN;
+    t_EH=t_CN;
 end
 
 
